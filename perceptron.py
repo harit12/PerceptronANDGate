99a5680062e3209f0d3d7c1a1e0f5a119a6195b9
harit12 <hmetla1245@gmail.com>
2020-11-20 18:34:34 -0500

    I think it may be impossible to use gradient descent

--- perceptron.py
+++ perceptron.py
@@ -1,91 +1,117 @@
 import numpy as np
 from matplotlib import pyplot as plt
-inputs = [[0,0],[0,1],[1,1],[1,0]]
-outputs = [1,0,1,0]
-inputs = np.array(inputs)
+inputs =np.array([[1.,0.,1.],[1.,1.,1.], [1., 1., 0.], [1., 0., 0.]])
+outputs = [0.,1.,0., 1.]
+#inputs = np.array(inputs)
+print(type(inputs))
 outputs = np.array(outputs)
 lr = .01
-epochs = 10000
+epochs = 25000
 def create_nn(n,m):
     """
     Initialize weights
+    N-number of nodes
+    m-number oflayers
     """
-    nueral_net = np.random.random((m,n))
+    nueral_net = np.random.random((m,n+1))
     return nueral_net
-
 def activation(zVal):
     """
     Activation function(sigmoid)
     """
     res = (1)/(1+np.exp(-zVal))
     return res
+
 def zVal(layer, weights):
     """
     Weights multiplied by previous layer or in this case, the inputs
     """
-    zVal = weights.dot(layer)
+    bias = weights[0] * layer[0]
+    zVal = weights[1:].dot(layer[1:])
+    zVal+=bias
     return zVal
+
 def forward(network, inputs):
     """
     Forward propogation
     Go though each layer of network and get the weights multiplied by the previous layer. Use activation function for z value for each nueron in each layer
     """
-    z = 0
+    activated = inputs
     for layer in network:
-        z = zVal(inputs, layer)
-    activated = activation(z)
-    return activated
+        z = zVal(activated, layer)
+        activated = activation(z)
+    print(activated)
+    return activated, z
 def cost(hypo, output):
     res = (hypo-output)**2
     return res
+def dCostdA(activated, output):
+    res = 2*(activated-output)
+    return res
+def dAdZ(zVal):
+    res = activation(zVal)*(1-activation(zVal))
+    return res
+def dCostdW(activated,z, inputs, output):
+    derivCost = dCostdA(activated, output)
+    derivAct = dAdZ(z)
+    zDeriv = inputs
+    res = derivCost*derivAct*zDeriv
+    return res
+def dCostdBias(activated, output):
+    derivCost = dCostdA(activated, output)
+    derivAct = dAdZ(activated)
+    zDeriv = 1
+    res = zDeriv*derivAct*derivCost
+    return res    
 def backprop(network, inputed, output):
-    activated = forward(network, inputed)
+    activated, zVal = forward(network, inputed)
     costed = cost(activated, output)
-    #print(costed, 'csot')
-    derivCost = activated-output
-    derivCost*=2.0
-    sigmoidDeriv = activated*(1-activated)
-    zDeriv = inputed
-    finalDeriv = derivCost*sigmoidDeriv*zDeriv
+    costdWeights = dCostdW(activated, zVal, inputed, output)
+    #biasDeriv = dCostdBias(activated, output)
+    #res = np.insert(costdWeights,0, biasDeriv, axis = 0)
     #print(zDeriv, 'zDeriv')
     #print(sigmoidDeriv, 'sDeriv')
     #print(derivCost, 'derivCost')
-    return finalDeriv, costed
+    return costed, costdWeights
+def update(gradient, weights):
+    weights = weights  + lr*(-gradient)
+    return weights
 def train(network, inputs, outputs):
     costList1 = []
     costList2 = []
     costList3 = []
     costList4 = []
     for n in range(epochs):
-        count = 0
-        finalDeriv = 0
-        for inputed in inputs:
-            finalDerived, cost = backprop(network, inputed, outputs[count])
-            finalDeriv+=finalDerived
-            count+=1
+        for count,(x,y) in enumerate(zip(inputs,outputs)):
+            x = np.array(x)
+            cost, finalDerived = backprop(network, x,y)
+            network=update(finalDerived, network)
             if count == 0:
                 costList1.append(cost)
             elif count ==1:
                 costList2.append(cost)
             elif count ==2:
                 costList3.append(cost)
-            elif count==3:
+            else:
                 costList4.append(cost)
-        finalDeriv = finalDeriv/4
         #print(finalDeriv, 'finalDeriv')
-        network-=finalDeriv*lr
         #print(network, 'net')
-    plt.plot(np.linspace(0,100, len(costList1)), costList1)
-    plt.plot(np.linspace(0,100, len(costList2)), costList2)
-    plt.plot(np.linspace(0,100, len(costList3)), costList3)
-    plt.plot(np.linspace(0,100, len(costList4)), costList4)
+    plt.plot(np.linspace(0,epochs, len(costList1)), costList1)
+    plt.plot(np.linspace(0,epochs, len(costList2)), costList2)
+    plt.plot(np.linspace(0,epochs, len(costList3)), costList3)
+    plt.plot(np.linspace(0,epochs, len(costList4)), costList4)
     return network
 def main():
     weights = create_nn(2,1)
-    print(weights, 'og')
+    print(weights)
     trained = train(weights, inputs, outputs)
     print(trained)
-    print(forward(trained, [1,1]))
+    print(forward(trained, [1.,0,1.]),'0,1')
+    print(forward(trained, [1.,1.,1.]),'1,1')
+    print(forward(trained, [1.,1.,0.]),'1,0')
+    print(forward(trained, [1.,0.,0.]),'0,0')
     plt.show()
-main()    
+
+if __name__=="__main__":
+    main()    
     
